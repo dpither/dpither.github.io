ade08936fcdeee56e872a4619b49ca61dac2cb9f
dpither <dylanpither@gmail.com>
2026-06-09 14:37:18 -0700

    added canonical link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,20 +3,18 @@
 	import favicon from '$lib/assets/icon.svg';
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
+	import { page } from '$app/state';
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<title>Dylan Pither</title>
 	<link rel="icon" type="image/svg+xml" href={favicon} />
-	<meta
-		name="description"
-		content="Hi, I'm Dylan Pither, a UBC Computer Engineering grad based in Vancouver. Checkout my projects and how to get in touch."
-	/>
+	<link rel="canonical" href="https://dpither.com{page.url.pathname}" />
 </svelte:head>
 
 <Header />
-<main class="border-border m-auto flex max-w-7xl flex-col">
+<main class="m-auto flex max-w-7xl flex-col">
 	{@render children?.()}
 </main>
 <Footer />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,5 +3,12 @@
 	import Projects from '$lib/components/Projects.svelte';
 </script>
 
+<svelte:head>
+	<meta
+		name="description"
+		content="Hi, I'm Dylan Pither, a UBC Computer Engineering grad based in Vancouver. Checkout my projects and how to get in touch."
+	/>
+</svelte:head>
+
 <Hero />
 <Projects />
